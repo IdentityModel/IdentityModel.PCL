053dcec3f01234433ca7981a161d3af92f544813
Dominick Baier <dbaier@leastprivilege.com>
2016-01-31 12:13:28 +0100

    changed license header

--- source/IdentityModel.Shared/OidcConstants.cs
+++ source/IdentityModel.Shared/OidcConstants.cs
@@ -1,4 +1,8 @@
-﻿namespace IdentityModel
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+namespace IdentityModel
 {
     public static class OidcConstants
     {
