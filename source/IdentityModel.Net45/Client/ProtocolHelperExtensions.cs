8c84743264f924cf4955048f8d132fca6876e245
Dominick Baier <dbaier@leastprivilege.com>
2016-02-02 12:01:30 +0100

    Add helper to create code challenge

---
+++ source/IdentityModel.Net45/Client/ProtocolHelperExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IdentityModel.Client
+{
+    public static class ProtocolHelperExtensions
+    {
+        public static string ToCodeChallenge(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return string.Empty;
+
+            using (var sha = SHA256.Create())
+            {
+                var bytes = Encoding.ASCII.GetBytes(input);
+                var hash = sha.ComputeHash(bytes);
+
+                return Base64Url.Encode(hash);
+            }
+        }
+    }
+}
