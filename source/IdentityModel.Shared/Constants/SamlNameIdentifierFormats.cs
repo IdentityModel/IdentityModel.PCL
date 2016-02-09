c47a86d468a7bd117ea046004752c31d2e48c3c6
Dominick Baier <dbaier@leastprivilege.com>
2016-02-09 09:24:01 +0100

    aligned assignments

--- source/IdentityModel.Shared/Constants/SamlNameIdentifierFormats.cs
+++ source/IdentityModel.Shared/Constants/SamlNameIdentifierFormats.cs
@@ -6,14 +6,14 @@ namespace IdentityModel.Constants
 {
     public static class SamlNameIdentifierFormats
     {
-        public const string EmailAddressString = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress";
-        public const string EncryptedString = "urn:oasis:names:tc:SAML:2.0:nameid-format:encrypted";
-        public const string EntityString = "urn:oasis:names:tc:SAML:2.0:nameid-format:entity";
-        public const string KerberosString = "urn:oasis:names:tc:SAML:2.0:nameid-format:kerberos";
-        public const string PersistentString = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
-        public const string TransientString = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient";
-        public const string UnspecifiedString = "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified";
+        public const string EmailAddressString               = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress";
+        public const string EncryptedString                  = "urn:oasis:names:tc:SAML:2.0:nameid-format:encrypted";
+        public const string EntityString                     = "urn:oasis:names:tc:SAML:2.0:nameid-format:entity";
+        public const string KerberosString                   = "urn:oasis:names:tc:SAML:2.0:nameid-format:kerberos";
+        public const string PersistentString                 = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
+        public const string TransientString                  = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient";
+        public const string UnspecifiedString                = "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified";
         public const string WindowsDomainQualifiedNameString = "urn:oasis:names:tc:SAML:1.1:nameid-format:WindowsDomainQualifiedName";
-        public const string X509SubjectNameString = "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName";
+        public const string X509SubjectNameString            = "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName";
     }
 }
