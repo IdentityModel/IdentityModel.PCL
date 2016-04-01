a95c9274fe0de95da52c342072084f5d495713f7
Brock Allen <brockallen@gmail.com>
2016-04-01 08:43:40 -0400

    sugar for Jwks

---
+++ source/IdentityModel.Net45/Jwt/JwkNetExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System.Security.Cryptography;
+
+namespace IdentityModel.Jwt
+{
+    public static class JwkNetExtensions
+    {
+        public static JsonWebKey ToJsonWebKey(this RSACryptoServiceProvider provider,
+            string alg = "RS256", string kid = null)
+        {
+            var key = provider.ExportParameters(false);
+
+            var n = Base64Url.Encode(key.Modulus);
+            var e = Base64Url.Encode(key.Exponent);
+            return new JsonWebKey()
+            {
+                N = n,
+                E = e,
+                Kid = kid ?? CryptoRandom.CreateUniqueId(),
+                Kty = "RSA",
+                Alg = alg,
+            };
+        }
+
+        public static RSACryptoServiceProvider CreateProvider(int keySize = 2048)
+        {
+            var csp = new CspParameters
+            {
+                Flags = CspProviderFlags.CreateEphemeralKey,
+                KeyNumber = (int)KeyNumber.Signature
+            };
+
+            return new RSACryptoServiceProvider(keySize, csp);
+        }
+    }
+}
---
+++ source/IdentityModel.Shared/Jwt/JwkExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Newtonsoft.Json;
+using System.Text;
+
+namespace IdentityModel.Jwt
+{
+    public static class JsonWebKeyExtensions
+    {
+        public static string ToJwkString(this IdentityModel.Jwt.JsonWebKey key)
+        {
+            var json = JsonConvert.SerializeObject(key);
+            // using UTF8 because we don't have ASCII in PCLs
+            return Base64Url.Encode(Encoding.UTF8.GetBytes(json));
+        }
+    }
+}
