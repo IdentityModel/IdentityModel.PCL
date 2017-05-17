0fa803eddafe3d2e2e0ae4be8f5f4795e6e03e49
Shimmy Weitzhandler <non@na.com>
2017-05-17 04:04:17 +0300

    Added DiscoveryClient.

---
+++ source/IdentityModel.Shared/Client/ResponseErrorType.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace IdentityModel.Client
+{
+  /// <summary>
+  /// Various reasons for a protocol endpoint error
+  /// </summary>
+  public enum ResponseErrorType
+  {
+    /// <summary>
+    /// none
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// protocol related
+    /// </summary>
+    Protocol,
+
+    /// <summary>
+    /// HTTP error
+    /// </summary>
+    Http,
+
+    /// <summary>
+    /// An exception occurred
+    /// </summary>
+    Exception,
+
+    /// <summary>
+    /// A policy violation
+    /// </summary>
+    PolicyViolation
+  }
+}
